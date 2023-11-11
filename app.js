2a9bb405bed1f6d732450c07315dbb7cca001ca5
danu <danujagreru@gmail.com>
2023-11-11 09:55:06 +0530

    install nodemon

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const port = 3000;
 
 app.get('/', (req, res) => {
     // console.log('Client is calling our server right now!');
-    res.send('<h1><center>Hello Express!</center></h1>');
+    // res.send('<h1><center>Hello Express!</center></h1>');
+    res.send('Hello Express!');
+});
+
+app.get('/customers', (req, res) => {
+   res.send('Customers page');
+});
+
+app.post('/', (req, res) => {
+    res.send('Got a POST request');
 });
 
 app.listen(port, () => {
